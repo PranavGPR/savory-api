ad7364bac046489e1453a64d8ac720640b74f355
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-15 19:18:26 +0530

    Get orders of a restaurant route tested

--- tests/integration/restaurant.test.js
+++ tests/integration/restaurant.test.js
@@ -99,4 +99,83 @@ describe('/restaurant/', () => {
 			expect(res.body).toHaveProperty('name');
 		});
 	});
+
+	describe('GET /order/:id', () => {
+		let token;
+
+		beforeEach(async () => {
+			id = 'test';
+			values.id = uuidv4();
+			token = generateBearerToken('restaurant');
+			await query('insert into customers(id,name,phoneNumber,email,password) values(?,?,?,?,?)', [
+				'9629ea6a-2854-4f5c-8501-a67e343837dd',
+				'Pranav',
+				'9750844039',
+				'pranav@email.com',
+				'Pranav@23'
+			]);
+			await query(
+				'insert into restaurants(id, menuid, name, phoneNumber, email, address, city, pincode, cuisines, opening_time, closing_time, popular_dishes, people_say, more_info, password) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+				Object.values(values)
+			);
+		});
+
+		afterEach(async () => {
+			await query('delete from orders');
+			await query('delete from customers');
+			await query('delete from restaurants');
+		});
+
+		const exec = () => {
+			return request(server).get(`/restaurant/order/${id}`).set('Authorization', token);
+		};
+
+		it('should return 400 if id is not valid', async () => {
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+			expect(res.body).toHaveProperty('error', 'Enter a valid id');
+		});
+
+		it('should return 401 if unauthorized', async () => {
+			token = '';
+			const res = await exec();
+
+			expect(res.status).toBe(401);
+		});
+
+		it('should return 404 if no orders found', async () => {
+			id = '77e39eec-a0a4-4efc-a971-bf0a8427aa88';
+			const res = await exec();
+
+			expect(res.status).toBe(404);
+			expect(res.body).toHaveProperty('error', 'No records found');
+		});
+
+		it('should return 200 if orders are found', async () => {
+			id = 'ecd6fdb7-2174-4b8c-9f36-cfc7982d866d';
+
+			let orderValues = {
+				customerid: '9629ea6a-2854-4f5c-8501-a67e343837dd',
+				restaurantid: 'ecd6fdb7-2174-4b8c-9f36-cfc7982d866d',
+				status: 'delivered',
+				delivered_on: '08:00:00',
+				ordered_item: JSON.stringify([
+					{ name: 'Idly', quantity: '4' },
+					{ name: 'Dosa', quantity: '1' }
+				]),
+				amount: 200,
+				payment_mode: 'COD'
+			};
+
+			await query(
+				'insert into orders(id, customerid, restaurantid, status,delivered_on, ordered_item, amount, payment_mode) values(?,?,?,?,?,?,?,?)',
+				[id, ...Object.values(orderValues)]
+			);
+
+			const res = await exec();
+
+			expect(res.status).toBe(200);
+		});
+	});
 });

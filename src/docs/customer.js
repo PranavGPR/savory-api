2b1e14dc981832d9184b54017a5a07e9ecbf2030
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-16 20:19:16 +0530

    Get details of a customer route docs added

--- src/docs/customer.js
+++ src/docs/customer.js
@@ -37,5 +37,42 @@ export default {
 				}
 			]
 		}
+	},
+	'/customer/{id}': {
+		get: {
+			tags: ['Customer'],
+			summary: 'Get details of a customer',
+			description: 'After logging in, get the details of a customer with their id',
+			responses: {
+				200: {
+					description: 'Details of the customer',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'path',
+					name: 'id',
+					description: 'ID needed to get details of a customer',
+					required: true
+				},
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				}
+			]
+		}
 	}
 };

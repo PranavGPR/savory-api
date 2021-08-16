1d723b4d44fac4088527f37e24a0c254b2ca5c97
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-16 09:58:40 +0530

    Ping router route url changed

--- src/tools/router.js
+++ src/tools/router.js
@@ -2,11 +2,11 @@ import { errorHandler } from 'middlewares';
 import { PingRouter, AdminRouter, CustomerRouter, RestaurantRouter, OrderRouter } from 'routers';
 
 export default function registerRouters(app) {
+	app.use('/', PingRouter);
 	app.use('/admin', AdminRouter);
 	app.use('/customer', CustomerRouter);
 	app.use('/restaurant', RestaurantRouter);
 	app.use('/order', OrderRouter);
-	app.use('/ping', PingRouter);
 
 	app.use(errorHandler);
 }

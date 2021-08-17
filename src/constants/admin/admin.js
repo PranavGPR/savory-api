69f5dec50ea681f66c2ac06f76200f0188d44224
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 12:05:01 +0530

    Admin examples created and imported in admin docs file

---
+++ src/constants/admin/admin.js
@@ -0,0 +1,14 @@
+export const adminLoginExample = {
+	email: 'pranavg@email.com',
+	password: 'Pranav@23'
+};
+
+export const createAdminExample = {
+	name: 'Pranav',
+	password: 'Pranav@23',
+	email: 'pranavg@email.com'
+};
+
+export const updateAdminExample = {
+	name: 'Pranav'
+};
--- src/docs/admin/admin.js
+++ src/docs/admin/admin.js
@@ -1,1 +1,10 @@
 import j2s from 'joi-to-swagger';
+
+import { adminLoginSchema, createAdminSchema, updateAdminSchema } from 'validators/admin';
+import { adminLoginExample, createAdminExample, updateAdminExample } from 'constants/admin';
+
+const { swagger: adminLoginSwagger } = j2s(adminLoginSchema);
+const { swagger: createAdminSwagger } = j2s(createAdminSchema);
+const { swagger: updateAdminSwagger } = j2s(updateAdminSchema);
+
+export default {};

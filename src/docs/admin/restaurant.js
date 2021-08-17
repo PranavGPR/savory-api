ad126715e48895e9b6ed5180ab4d1c0088e18f9c
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:30:47 +0530

    Delete restaurant route added to docs

--- src/docs/admin/restaurant.js
+++ src/docs/admin/restaurant.js
@@ -201,6 +201,44 @@ export default {
 					required: true
 				}
 			]
+		},
+		delete: {
+			tags: ['Admin/Restaurant'],
+			summary: 'Delete a Restaurant',
+			description: 'Delete Restaurants by id',
+			responses: {
+				200: {
+					description: 'Restaurant deleted',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				},
+				{
+					in: 'path',
+					name: 'id',
+					description: 'ID of the Restaurant needed to delete that Restaurant',
+					required: true
+				}
+			]
 		}
 	}
 };

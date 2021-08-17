3277435883a63b4b82101d16016663e0f2e73e8b
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:15:34 +0530

    Get all orders route added to docs

--- src/docs/admin/order.js
+++ src/docs/admin/order.js
@@ -5,4 +5,39 @@ import { updateOrderExample } from 'constants/admin';
 
 const { swagger: updateOrderSwagger } = j2s(updateOrderSchema);
 
-export default {};
+export default {
+	'/admin/order/all': {
+		get: {
+			tags: ['Admin/Order'],
+			summary: 'Get all orders',
+			description: 'After logging in, get all order',
+			responses: {
+				200: {
+					description: 'Details of the orders',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				}
+			]
+		}
+	}
+};

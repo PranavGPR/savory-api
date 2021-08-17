e70d1554582a8f0414461a85da8d3ffee0309448
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:26:19 +0530

    Create restaurant route added to docs

--- src/docs/admin/restaurant.js
+++ src/docs/admin/restaurant.js
@@ -6,4 +6,45 @@ import { createRestaurantExample, updateRestaurantExample } from 'constants/admi
 const { swagger: createRestaurantSwagger } = j2s(createRestaurantSchema);
 const { swagger: updateRestaurantSwagger } = j2s(updateRestaurantSchema);
 
-export default {};
+export default {
+	'/admin/restaurant/create': {
+		post: {
+			tags: ['Admin/Restaurant'],
+			summary: 'Create a restaurant as an admin',
+			description: 'Create restaurant after logging in as an admin',
+			responses: {
+				200: {
+					description: `Restaurant created`,
+					content: 'application/json'
+				},
+				400: {
+					description: 'Error in inserting the values'
+				},
+				401: {
+					description: 'Access denied'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'path',
+					name: 'token',
+					description: 'Token for Authorization',
+					required: true
+				},
+				{
+					in: 'body',
+					name: 'body',
+					description: 'Details needed to create a restaurant',
+					required: true,
+					schema: {
+						...createRestaurantSwagger,
+						example: createRestaurantExample
+					}
+				}
+			]
+		}
+	}
+};

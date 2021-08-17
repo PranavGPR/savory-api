bf025f3ad78001ebecfa37ed43d769d0fd5b8726
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 12:14:00 +0530

    Admin login route added in docs

--- src/docs/admin/admin.js
+++ src/docs/admin/admin.js
@@ -7,4 +7,36 @@ const { swagger: adminLoginSwagger } = j2s(adminLoginSchema);
 const { swagger: createAdminSwagger } = j2s(createAdminSchema);
 const { swagger: updateAdminSwagger } = j2s(updateAdminSchema);
 
-export default {};
+export default {
+	'/admin/login': {
+		post: {
+			tags: ['Admin'],
+			summary: 'Login as admin',
+			description: 'Login to the server as a admin to manage orders.',
+			responses: {
+				200: {
+					description: `You're logged in`,
+					content: 'application/json'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'body',
+					name: 'body',
+					description: 'Details needed to login as a admin',
+					required: true,
+					schema: {
+						...adminLoginSwagger,
+						example: adminLoginExample
+					}
+				}
+			]
+		}
+	}
+};

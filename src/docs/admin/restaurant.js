0927c6bd44fc64ae1e78fb8c989e1e043fbe87f5
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:27:09 +0530

    Get all restaurants route added to docs

--- src/docs/admin/restaurant.js
+++ src/docs/admin/restaurant.js
@@ -46,5 +46,39 @@ export default {
 				}
 			]
 		}
+	},
+	'/admin/restaurant/all': {
+		get: {
+			tags: ['Admin/Restaurant'],
+			summary: 'Get all restaurants',
+			description: 'After logging in, get all restaurants',
+			responses: {
+				200: {
+					description: 'Details of the restaurants',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				}
+			]
+		}
 	}
 };

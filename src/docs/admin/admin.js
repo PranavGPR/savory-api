5d241ebc1ca5cb48acc42534acf71bf8ac8abf44
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 12:25:05 +0530

    Get all admins route added in docs

--- src/docs/admin/admin.js
+++ src/docs/admin/admin.js
@@ -78,5 +78,39 @@ export default {
 				}
 			]
 		}
+	},
+	'/admin/all': {
+		get: {
+			tags: ['Admin'],
+			summary: 'Get all admins',
+			description: 'After logging in, get all admins',
+			responses: {
+				200: {
+					description: 'Details of the orders',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				}
+			]
+		}
 	}
 };

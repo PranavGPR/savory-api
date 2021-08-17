03f16c3228913df173c1a45699d9289e0135f328
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:20:18 +0530

    Schema extracted from validator function

--- src/validators/admin/restaurant.js
+++ src/validators/admin/restaurant.js
@@ -1,55 +1,53 @@
 import Joi from 'joi';
 
+export const createRestaurantSchema = Joi.object({
+	name: Joi.string()
+		.required()
+		.pattern(/^[a-zA-Z]+$/),
+	email: Joi.string()
+		.email({ tlds: { allow: false } })
+		.required(),
+	phoneNumber: Joi.string()
+		.length(10)
+		.pattern(/^[0-9]+$/)
+		.required(),
+	address: Joi.string().required(),
+	city: Joi.string().min(5).max(32).required(),
+	pincode: Joi.string()
+		.length(6)
+		.pattern(/^[0-9]+$/)
+		.required(),
+	cuisines: Joi.string().required(),
+	opening_time: Joi.string()
+		.pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/)
+		.required(),
+	closing_time: Joi.string()
+		.pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/)
+		.required(),
+	popular_dishes: Joi.string().required(),
+	people_say: Joi.string().required(),
+	more_info: Joi.string().required(),
+	password: Joi.string().min(8).max(256).required()
+});
 export const createRestaurantValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string()
-			.required()
-			.pattern(/^[a-zA-Z]+$/),
-		email: Joi.string()
-			.email({ tlds: { allow: false } })
-			.required(),
-		phoneNumber: Joi.string()
-			.length(10)
-			.pattern(/^[0-9]+$/)
-			.required(),
-		address: Joi.string().required(),
-		city: Joi.string().min(5).max(32).required(),
-		pincode: Joi.string()
-			.length(6)
-			.pattern(/^[0-9]+$/)
-			.required(),
-		cuisines: Joi.string().required(),
-		opening_time: Joi.string()
-			.pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/)
-			.required(),
-		closing_time: Joi.string()
-			.pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/)
-			.required(),
-		popular_dishes: Joi.string().required(),
-		people_say: Joi.string().required(),
-		more_info: Joi.string().required(),
-		password: Joi.string().min(8).max(256).required()
-	});
-
-	return schema.validate(data);
+	return createRestaurantSchema.validate(data);
 };
 
+export const updateRestaurantSchema = Joi.object({
+	name: Joi.string(),
+	phoneNumber: Joi.string(),
+	address: Joi.string(),
+	city: Joi.string().min(5).max(32),
+	pincode: Joi.string()
+		.length(6)
+		.pattern(/^[0-9]+$/),
+	cuisines: Joi.string(),
+	opening_time: Joi.string().pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/),
+	closing_time: Joi.string().pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/),
+	popular_dishes: Joi.string(),
+	people_say: Joi.string(),
+	more_info: Joi.string()
+});
 export const updateRestaurantValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string(),
-		phoneNumber: Joi.string(),
-		address: Joi.string(),
-		city: Joi.string().min(5).max(32),
-		pincode: Joi.string()
-			.length(6)
-			.pattern(/^[0-9]+$/),
-		cuisines: Joi.string(),
-		opening_time: Joi.string().pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/),
-		closing_time: Joi.string().pattern(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/),
-		popular_dishes: Joi.string(),
-		people_say: Joi.string(),
-		more_info: Joi.string()
-	});
-
-	return schema.validate(data);
+	return updateRestaurantSchema.validate(data);
 };

dc1894673ddec67c35d60ee3a41d3d380fc83b1d
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 11:53:18 +0530

    Schema extracted from validator function

--- src/validators/admin/admin.js
+++ src/validators/admin/admin.js
@@ -1,32 +1,32 @@
 import Joi from 'joi';
 
-export const createAdminValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string().required(),
-		password: Joi.string().min(8).max(256).required(),
-		email: Joi.string()
-			.email({ tlds: { allow: false } })
-			.required()
-	});
+export const createAdminSchema = Joi.object({
+	name: Joi.string().required(),
+	password: Joi.string().min(8).max(256).required(),
+	email: Joi.string()
+		.email({ tlds: { allow: false } })
+		.required()
+});
 
-	return schema.validate(data);
+export const createAdminValidator = data => {
+	return createAdminSchema.validate(data);
 };
+
+export const updateAdminSchema = Joi.object({
+	name: Joi.string().required()
+});
 
 export const updateAdminValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string().required()
-	});
-
-	return schema.validate(data);
+	return updateAdminSchema.validate(data);
 };
 
-export const adminLoginValidator = data => {
-	const schema = Joi.object({
-		email: Joi.string()
-			.email({ tlds: { allow: false } })
-			.required(),
-		password: Joi.string().min(8).max(256).required()
-	});
+export const adminLoginSchema = Joi.object({
+	email: Joi.string()
+		.email({ tlds: { allow: false } })
+		.required(),
+	password: Joi.string().min(8).max(256).required()
+});
 
-	return schema.validate(data);
+export const adminLoginValidator = data => {
+	return adminLoginSchema.validate(data);
 };

ee4bbb7a7b48e4a6a8f007dc760bb909221dac93
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 18:49:28 +0530

    Customer schema seperated from validator function

--- src/validators/admin/customer.js
+++ src/validators/admin/customer.js
@@ -1,40 +1,40 @@
 import Joi from 'joi';
 
-export const createCustomerValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string().required(),
-		password: Joi.string().min(8).max(256).required(),
-		email: Joi.string()
-			.email({ tlds: { allow: false } })
-			.required(),
-		phoneNumber: Joi.string()
-			.length(10)
-			.pattern(/^[0-9]+$/)
-			.required(),
-		address: Joi.string().empty('').default(null),
-		city: Joi.string().min(5).max(32).empty('').default(null),
-		pincode: Joi.string()
-			.length(6)
-			.pattern(/^[0-9]+$/)
-			.empty('')
-			.default(null)
-	});
+export const createCustomerSchema = Joi.object({
+	name: Joi.string().required(),
+	password: Joi.string().min(8).max(256).required(),
+	email: Joi.string()
+		.email({ tlds: { allow: false } })
+		.required(),
+	phoneNumber: Joi.string()
+		.length(10)
+		.pattern(/^[0-9]+$/)
+		.required(),
+	address: Joi.string().empty('').default(null),
+	city: Joi.string().min(5).max(32).empty('').default(null),
+	pincode: Joi.string()
+		.length(6)
+		.pattern(/^[0-9]+$/)
+		.empty('')
+		.default(null)
+});
 
-	return schema.validate(data);
+export const createCustomerValidator = data => {
+	return createCustomerSchema.validate(data);
 };
 
-export const updateCustomerValidator = data => {
-	const schema = Joi.object({
-		name: Joi.string(),
-		phoneNumber: Joi.string()
-			.length(10)
-			.pattern(/^[0-9]+$/),
-		address: Joi.string(),
-		city: Joi.string().min(5).max(32),
-		pincode: Joi.string()
-			.length(6)
-			.pattern(/^[0-9]+$/)
-	});
+export const updateCustomerSchema = Joi.object({
+	name: Joi.string(),
+	phoneNumber: Joi.string()
+		.length(10)
+		.pattern(/^[0-9]+$/),
+	address: Joi.string(),
+	city: Joi.string().min(5).max(32),
+	pincode: Joi.string()
+		.length(6)
+		.pattern(/^[0-9]+$/)
+});
 
-	return schema.validate(data);
+export const updateCustomerValidator = data => {
+	return updateCustomerSchema.validate(data);
 };

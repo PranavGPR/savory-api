5046e28101fe9780639a4b3596e2d1bede93dfc2
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:18:22 +0530

    Delete order route added to docs

--- src/docs/admin/order.js
+++ src/docs/admin/order.js
@@ -126,6 +126,44 @@ export default {
 					required: true
 				}
 			]
+		},
+		delete: {
+			tags: ['Admin/Order'],
+			summary: 'Delete an order',
+			description: 'Delete orders by id',
+			responses: {
+				200: {
+					description: 'Order deleted',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				},
+				{
+					in: 'path',
+					name: 'id',
+					description: 'ID of the order needed to delete that order',
+					required: true
+				}
+			]
 		}
 	}
 };

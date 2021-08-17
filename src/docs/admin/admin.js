9921238b4d34f301dfc74b3871755c4fe7010f49
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 18:47:11 +0530

    Get an admin by id route added in docs

--- src/docs/admin/admin.js
+++ src/docs/admin/admin.js
@@ -114,6 +114,44 @@ export default {
 		}
 	},
 	'/admin/{id}': {
+		get: {
+			tags: ['Admin'],
+			summary: 'Get an admin',
+			description: 'Get details of an admin by their id',
+			responses: {
+				200: {
+					description: 'Details of an admin',
+					content: 'application/json'
+				},
+				400: {
+					description: 'Enter a valid id'
+				},
+				401: {
+					description: 'Access Denied'
+				},
+				404: {
+					description: 'No records found'
+				},
+				500: {
+					description: 'Internal Server error'
+				}
+			},
+			parameters: [
+				{
+					in: 'path',
+					name: 'id',
+					description: 'ID of the admin needed to get details',
+					required: true
+				},
+				{
+					in: 'header',
+					name: 'Authorization',
+					description: 'Token for authorization',
+					required: true,
+					type: 'string'
+				}
+			]
+		},
 		put: {
 			tags: ['Admin'],
 			summary: 'Update name of admin',

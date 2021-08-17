c2a3b2c82b35a9d7daf7b842fd48a159f7e9930d
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 19:23:06 +0530

    Restaurant examples created

---
+++ src/constants/admin/restaurant.js
@@ -0,0 +1,19 @@
+export const createRestaurantExample = {
+	name: 'Restaurant',
+	phoneNumber: '9750844040',
+	email: 'restaurant2@email.com',
+	address: '4/1252, Street',
+	city: 'Madurai',
+	pincode: '625020',
+	cuisines: JSON.stringify(['Chinese', 'North Indian', 'South Indian']),
+	opening_time: '2021-06-15 08:00:00',
+	closing_time: '2021-06-15 20:00:00',
+	popular_dishes: JSON.stringify(['Tikka Gravy', 'Paneer Masala']),
+	people_say: JSON.stringify(['Good service']),
+	more_info: JSON.stringify(['Valet Parking']),
+	password: '12345678'
+};
+
+export const updateRestaurantExample = {
+	name: 'Hotel Saravana Bhavan'
+};

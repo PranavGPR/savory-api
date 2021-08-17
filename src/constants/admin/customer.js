6213de5f044a74c94804e2fa3ef2858deb12ee7d
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-08-17 18:52:47 +0530

    Example constants created and imported

---
+++ src/constants/admin/customer.js
@@ -0,0 +1,11 @@
+export const createCustomerExample = {
+	name: 'Pranav',
+	phoneNumber: '9750844040',
+	email: 'pranavabc@email.com',
+	password: '12345678'
+};
+
+export const updateCustomerExample = {
+	phoneNumber: '9750844039',
+	name: 'Pranav'
+};
--- src/docs/admin/customer.js
+++ src/docs/admin/customer.js
@@ -1,6 +1,7 @@
 import j2s from 'joi-to-swagger';
 
 import { createCustomerSchema, updateCustomerSchema } from 'validators/admin';
+import { createCustomerExample, updateCustomerExample } from 'constants/admin';
 
 const { swagger: createCustomerSwagger } = j2s(createCustomerSchema);
 const { swagger: updateCustomerSwagger } = j2s(updateCustomerSchema);

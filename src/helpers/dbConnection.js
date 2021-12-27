855a0450ce2fb723b28ec2403a0a8925787dc6d8
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-12-27 12:56:48 +0530

    Included ssl and test configurations

--- src/helpers/dbConnection.js
+++ src/helpers/dbConnection.js
@@ -2,24 +2,36 @@ import mysql from 'mysql2/promise';
 import 'regenerator-runtime/runtime';
 import 'dotenv/config';
 
-const { DB_USER, DB_PASS, DB_URL, DB_NAME, TEST_DB_NAME, NODE_ENV } = process.env;
+const {
+	DB_USER,
+	DB_PASS,
+	DB_URL,
+	DB_NAME,
+	TEST_DB_NAME,
+	TEST_DB_PASS,
+	TEST_DB_URL,
+	TEST_DB_USER,
+	NODE_ENV
+} = process.env;
 
 export const query = async (sql, params) => {
 	let connection;
 
 	if (NODE_ENV === 'test') {
 		connection = await mysql.createConnection({
-			host: DB_URL,
-			user: DB_USER,
-			password: DB_PASS,
-			database: TEST_DB_NAME
+			host: TEST_DB_URL,
+			user: TEST_DB_USER,
+			password: TEST_DB_PASS,
+			database: TEST_DB_NAME,
+			ssl: {}
 		});
 	} else {
 		connection = await mysql.createConnection({
 			host: DB_URL,
 			user: DB_USER,
 			password: DB_PASS,
-			database: DB_NAME
+			database: DB_NAME,
+			ssl: {}
 		});
 	}
 
